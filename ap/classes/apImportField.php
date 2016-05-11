dffb240e76ef13a965991edd840c4f67a3d6f69f
Toni Rosa <rosa.toni@apfutura.net>
2016-05-11 16:26:03 -0700

    New: builtIn converstion for dates from dd/mm/yyyy to yyyy/mm/dd (postgres's date default)

--- ap/classes/apImportField.php
+++ ap/classes/apImportField.php
@@ -36,10 +36,14 @@ class apImportField
                 $message = "Required field '".$this->name. "' is empty";
             }
         } else {
-            if ($this->builtInChecks) {   // If builtin check are in place this will be made, and will NOT continue to the custom ones if not meet
-              $this->_doBuiltInChecks($input, $result, $data, $message);
-              if ($result) $this->_applyCustomImportFunction($input, $result, $data, $message);
-            } else $this->_applyCustomImportFunction($input, $result, $data, $message);
+            if (strlen($input) > 0) {
+              if ($this->builtInChecks) {   // If builtin check are in place this will be made, and will NOT continue to the custom ones if not meet
+                $this->_doBuiltInChecks($input, $result, $data, $message);
+                if ($result) $this->_applyCustomImportFunction($data, $result, $data, $message); // First parameter is $data and not $input because _doBuiltInChecks can modify de default input
+              } else $this->_applyCustomImportFunction($input, $result, $data, $message);
+            } else {
+              $this->_applyCustomImportFunction($input, $result, $data, $message);
+            }
         }
         return ["result" => $result, "data" => $data, "message" => $message];
     }
@@ -71,10 +75,28 @@ class apImportField
     {
         if (array_key_exists("is_numeric", $this->builtInChecks) && $this->builtInChecks['is_numeric']==true) {
             $result = is_numeric($input);
+            $data = $input;
+            if (!$result) $message = "Check is_numeric error for field '".$this->name. "' and value '" . $input . "'";
+        } else if (array_key_exists("dateto_yyyymmdd", $this->builtInChecks) && $this->builtInChecks['dateto_yyyymmdd']==true)  {
+            $result = $this->_dateto_yyyymmdd($input);
             $data = $result;
-            if (!$result) $message = "Check is_numeric error for field '".$this->name. "' and value '" . $data . "'";
+            if (!$result) $message = "Check date error for field '".$this->name. "' and value '" . $input . "'";
         }
         return;
+    }
+
+    private function _dateto_yyyymmdd($date){
+      $date = str_replace(array('.', '-', '\\\\'), '/', $date);
+  		$dateParts = explode("/", $date);
+  		$result = false;
+  		if (count($dateParts)==3 &&
+  				is_numeric($dateParts[0]) && is_numeric($dateParts[1]) && is_numeric($dateParts[2]) &&
+  				$dateParts[0] > 0 && $dateParts[0] < 32 &&
+  				$dateParts[1] > 0 && $dateParts[1] < 13 &&
+  				$dateParts[2] > 1000 && $dateParts[2] < 9999) {
+  					$result = $dateParts[2] . '/' . $dateParts[1]			. '/' . $dateParts[0];
+  		}
+      return $result;
     }
 
 }
