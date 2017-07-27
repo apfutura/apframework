9d76f879d6c221f30ccc8fdc4dd0309be5e2a7f7
adri <marti.adria@apfutura.net>
2017-07-27 17:53:06 +0200

    Fix: php7 requires to explicit all parameters. Corrected some functions adding default parameter values

--- ap/lib/apHtmlUtils.php
+++ ap/lib/apHtmlUtils.php
@@ -90,7 +90,7 @@ class apHtmlUtils {
 		return $htmlCombo;
 	}
 	
-	static function comboHtml_fromArray($name, $array_values = array(""),$array_label = array(""),$seleccionat,$onchange='',$html_class='',$html_stlye='', $option_generic_attributes = null, $array_attributes_select = array(), $array_row_attributes = array()) {
+	static function comboHtml_fromArray($name, $array_values = array(""),$array_label = array(""),$seleccionat = null,$onchange='',$html_class='',$html_stlye='', $option_generic_attributes = null, $array_attributes_select = array(), $array_row_attributes = array()) {
 		if (strlen($html_class)==0){
 			$html_class = 'stylishCombo';
 		}		
@@ -124,7 +124,7 @@ class apHtmlUtils {
 		return $retorna;
 	}	
 	
-	static function comboHtml_fromTable($name,$camp,$camp_id,$text_inicial_combo,$valor_inicial,$taula,$where,$ordre,$seleccionada,$onclick,$html_class='',$html_stlye='', $array_attributes = null, $array_attributes_select = array() ) {
+	static function comboHtml_fromTable($name,$camp,$camp_id,$text_inicial_combo,$valor_inicial,$taula,$where,$ordre,$seleccionada = null,$onclick = null,$html_class='',$html_stlye='', $array_attributes = null, $array_attributes_select = array() ) {
 		if (strlen($html_class)==0){
 			$html_class = 'stylishCombo';
 		}
